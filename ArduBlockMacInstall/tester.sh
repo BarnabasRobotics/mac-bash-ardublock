c343c3ddfe51ad52d7150b68e6ee75bb8762e480
Edward Li <edward@barnabasrobotics.com>
2020-05-22 10:15:27 -0700

    updated for v1.5 ch34x driver

--- ArduBlockMacInstall/tester.sh
+++ ArduBlockMacInstall/tester.sh
@@ -1,6 +1,6 @@
-if test -f "./CH34x_nstall_V1.3.pkg"; then
+if test -f "./CH34x_nstall_V1.5.pkg"; then
     continue
-elif test -f "./CH34x_Install_V1.3.zip"; then
+elif test -f "./CH34x_Install_V1.5.zip"; then
     echo "\nUnzipping Driver"
 else
     echo "CH34x files are missing. Driver not installed."
